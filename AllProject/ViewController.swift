5fa31869f97ad0761e161576e3b86e59ba17a422
liouyanting <msze5189@gmail.com>
2018-06-24 00:24:58 +0800

    Initial Commit

---
+++ AllProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllProject
+//
+//  Created by 劉彥廷 on 2018/6/24.
+//  Copyright © 2018年 liou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
