fb017a8a52a61621106a2bf04c7402a20637e8f8
liouyanting <msze5189@gmail.com>
2018-06-27 17:27:38 +0800

    新增 - 特殊需求變化

--- AllProject/CallCar/CallCarMainViewController.swift
+++ AllProject/CallCar/CallCarMainViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class CallCarMainViewController: UIViewController {
 
     @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var phone: UITextField!
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var aliasLocationName: UILabel!
     @IBOutlet weak var oftenUseLocationName: UILabel!
     @IBOutlet weak var oftenLocationViewHeigth: NSLayoutConstraint!
@@ -19,33 +22,82 @@ class CallCarMainViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var wheelChair: UIImageView!
-    @IBOutlet weak var largeLuggag: UIImageView!
+    @IBOutlet weak var largeLuggage: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+         wheelChair.tag = 0
+         largeLuggage.tag = 0
+        
+        
+        //show keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
+
+        // 特殊需求 btn
+         let wheelChairBtnaddGester = UITapGestureRecognizer.init(target: self, action: #selector(wheelChairBtn(_:)))
+
+        let largeLuggageBtnaddGester = UITapGestureRecognizer.init(target: self, action: #selector(luggageBtn))
+        
+        wheelChair.addGestureRecognizer(wheelChairBtnaddGester)
+        largeLuggage.addGestureRecognizer(largeLuggageBtnaddGester)
+        
+        largeLuggage.isUserInteractionEnabled = true
+        wheelChair.isUserInteractionEnabled = true
     }
 
+    @objc func wheelChairBtn(_ sender: UITapGestureRecognizer){
+       
+            if wheelChair.tag == 0 {
+                wheelChair.image =  UIImage(named: "icon_checkbox_ov")
+                wheelChair.tag = 1
+            } else {
+                wheelChair.image = UIImage.init(named: "icon_checkbox_off")
+                wheelChair.tag = 0
+            }
+    }
+    
+    @objc func luggageBtn() {
+        if largeLuggage.tag == 0 {
+            largeLuggage.image =  UIImage(named: "icon_checkbox_ov")
+            largeLuggage.tag = 1
+        } else {
+            largeLuggage.image = UIImage.init(named: "icon_checkbox_off")
+            largeLuggage.tag = 0
+        }
+    }
+    
+    // 複寫（當碰觸螢幕）這裡沒反應
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-   //摸螢幕縮鍵盤
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.backgroundView.endEditing(true)
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
     }
     
+    // MARK: Keyboard Notifications
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardHeight = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
+            scrollView.contentInset = UIEdgeInsetsMake(0, 0, keyboardHeight , 0)
+            scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, keyboardHeight , 0)
+            
+        }
     }
-    */
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.scrollView.contentInset = UIEdgeInsetsMake((navigationController?.navigationBar.frame.size.height)! + 20.0, 0, 0, 0)
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsetsMake((navigationController?.navigationBar.frame.size.height)! + 20.0, 0, 0, 0)
+    }
+
+    
 
 }
