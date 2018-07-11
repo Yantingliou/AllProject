0502ab15fd5b6f733727d212620dcc8b8afa9885
liouyanting <msze5189@gmail.com>
2018-07-11 16:45:30 +0800

    Update

--- AllProject/CallCar/SearchCarViewController.swift
+++ AllProject/CallCar/SearchCarViewController.swift
@@ -34,14 +34,10 @@ class SearchCarViewController: UIViewController {
     }
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func carArrive(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "CallResultViewController") as! CallResultViewController
+        self.navigationController?.pushViewController(vc, animated: true)
     }
-    */
+   
 
 }
